da9d69be2250fe75164dd5f4a91409a410ab1648
degenerat3 <james.maskelony@gmail.com>
2019-06-23 19:21:30 -0400

    define classes/tables

--- core/database.py
+++ core/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 Base = declarative_base()
 
 class Host(Base):
@@ -23,6 +24,38 @@ class Bot(Base):
     hostid = Column(Integer, ForeignKey('hosts.id'))
 
     def __repr__(self):
-        return "Bot(id='%s', uuid='%s', interval='%d', delta='%d', hostid='%d')>" % (self.id, self.uuid, self.interval, self.delta, self.hostid)
+        return "<Bot(id='%s', uuid='%s', interval='%d', delta='%d', hostid='%d')>" % (self.id, self.uuid, self.interval, self.delta, self.hostid)
 
 
+class Group(Base):
+    __tablename__ = 'groups'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    def __repr__(self):
+        return "<Group(id='%d', name='%s')>" % (self.id, self.name)
+
+class Action(Base):
+    __tablename__ = 'actions'
+
+    id = Column(Integer, primary_key=True)
+    mode = Column(String)
+    arguments = Column(String)
+    options = Column(String)
+    queued = Column(Boolean)
+    responded = Column(Boolean)
+    hostid = Column(Integer, ForeignKey('host.id'))
+
+    def __repr__(self):
+        return "<Action(id='%d', mode='%s', arguments='%s', options='%s', queued='%s', responded='%s', hostid='%d')>" % self.id, self.mode, self.arguments, self.options, self.queued, self.responded, self.hostid)
+    
+class Response(Base):
+    __tablename__ = 'responses'
+
+    id = Column(Integer, primary_key=True)
+    data = Column(String)
+    actionid = Column(Integer, ForeignKey('action.id'))
+
+    def __repr__(self):
+        return "<Respnose(id='%d', data='%s', actionid='%d')>" % (self.id, self.data, self.actionid)
