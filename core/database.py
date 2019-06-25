2ab6f15daf6c45cd962add05d5935f1e9e17c72a
degenerat3 <james.maskelony@gmail.com>
2019-06-24 22:22:59 -0400

    fix base/engine init order

--- core/database.py
+++ core/database.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import sessionmaker
 
 
 Base = declarative_base()
-Base.metadata.create_all(engine)
 engine = create_engine('sqlite:////tmp/test.db', echo=True)
+Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
