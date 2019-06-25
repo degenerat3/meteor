b55b5066727a31c08b7434c4f4f0d42caf96c469
degenerat3 <james.maskelony@gmail.com>
2019-06-24 21:55:01 -0400

    init db session

--- core/database.py
+++ core/database.py
@@ -1,6 +1,11 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import sessionmaker
 Base = declarative_base()
+
+engine = create_engine('sqlite:////tmp/test.db', echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
 
 class Host(Base):
     __tablename__ = 'hosts'
