38ea10c3c01bd272e35922575e6deb5f4bc76ef2
degenerat3 <james.maskelony@gmail.com>
2019-06-26 23:05:21 -0400

    test input fix

--- core/test_commander.py
+++ core/test_commander.py
@@ -3,17 +3,17 @@ import requests
 server = "http://localhost:9999"
 
 header = {'Content-type': 'application/json'}
-data = {groupname: "Webservers"}
+data = {"groupname": "Webservers"}
 request = requests.post(server + "/register/group", headers=header, data=json.dumps(data))
 print(request.body)
 
 header = {'Content-type': 'application/json'}
-data = {hostname: "192.69.4.20", interface: "eth69", groupname: "Webservers"}
+data = {"hostname": "192.69.4.20", "interface": "eth69", "groupname": "Webservers"}
 request = requests.post(server + "/register/host", headers=header, data=json.dumps(data))
 print(request.body)
 
 header = {'Content-type': 'application/json'}
-data = {uuid: "aklsjflasdjfl", interval: 60, delta: 5, hostname: "192.69.4.20"}
+data = {"uuid": "aklsjflasdjfl", "interval": 60, "delta": 5, "hostname": "192.69.4.20"}
 request = requests.post(server + "/register/bot", headers=header, data=json.dumps(data))
 print(request.body)
 
