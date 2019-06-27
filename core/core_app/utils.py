4b5046b90364bc202df6a03ad903d1b7fcba5504
degenerat3 <james.maskelony@gmail.com>
2019-06-26 23:10:29 -0400

    query fixes

--- core/core_app/utils.py
+++ core/core_app/utils.py
@@ -2,7 +2,7 @@ from .database import *
 
 def registerBot(uuid, interval, delta, hostname):
     print("registering bot")
-    q = session.query.filter(Host.hostname == hostname).one()
+    q = session.query().filter(Host.hostname == hostname).one()
     hostid = q.id
     b = Bot(uuid, interval, delta, hostid)
     return [True, "None"]
@@ -10,7 +10,7 @@ def registerBot(uuid, interval, delta, hostname):
 
 def registerHost(hostname, interface, groupname):
     print("registering host")
-    q = session.query.filter(Group.name == groupname).one()
+    q = session.query().filter(Group.name == groupname).one()
     groupid = q.id
     h = Host(hostname, interface, groupid)
     return [True, "None"]
@@ -25,17 +25,17 @@ def registerGroup(groupname):
 def dumpDatabase():
     data = "HOSTS:\n"
     for instance in session.query(Host).order_by(Host.id):
-        data += instance + "\n"
+        data += str(instance) + "\n"
     data += "\nBOTS:\n"
     for instance in session.query(Bot).order_by(Bot.id):
-        data += instance + "\n"
+        data += str(instance) + "\n"
     data += "\nGROUPS:\n"
     for instance in session.query(Group).order_by(Group.id):
-        data += instance + "\n"
+        data += (instance) + "\n"
     data += "\nACTIONS:\n"
     for instance in session.query(Action).order_by(Action.id):
-        data += instance + "\n"
+        data += (instance) + "\n"
     data += "\nRESPONSES:\n"
     for instance in session.query(Response).order_by(Response.id):
-        data += instance + "\n"
+        data += (instance) + "\n"
     return data
--- core/test_commander.py
+++ core/test_commander.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 server = "http://localhost:9999"
 
