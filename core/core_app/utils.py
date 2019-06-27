e44bbbfee27860eb2222e3a73535fc133df0678f
degenerat3 <james.maskelony@gmail.com>
2019-06-26 21:43:40 -0400

    register views done

--- core/core_app/utils.py
+++ core/core_app/utils.py
@@ -1,5 +1,15 @@
 from .database import *
 
 def registerBot(uuid, interval, delta, hostname):
-    print("registering user")
+    print("registering bot")
+    return [True, "None"]
+
+
+def registerHost(hostname, interface, groupname):
+    print("registering host")
+    return [True, "None"]
+
+
+def registerGroup(groupname):
+    print("registering group")
     return [True, "None"]
--- core/core_app/views.py
+++ core/core_app/views.py
@@ -8,17 +8,38 @@ def index():
     return "Meteor core is running.\n"
 
 
-@app.route('/register', methods=['POST'])
-def process_campfire():
+@app.route('/register/bot', methods=['POST'])
+def newbot():
     content = request.json
     uuid = content['uuid']
     interval = content['interval']
     delta = content['delta']
     hostname = content['hostname']
-
     reg_status = registerBot(uuid, interval, delta, hostname)
     if reg_status[0]:
         return "Success"
+    failure_str = "Register failure: " + reg_status[1]
+    return failure_str
 
+
+@app.route('/register/host', methods=['POST'])
+def newhost():
+    content = request.json
+    hostname = content['hostname']
+    interface = content['interface']
+    groupname = content['groupname']
+    reg_status = registerHost(hostname, interface, groupname)
+    if reg_status[0]:
+        return "Success"
+    failure_str = "Register failure: " + reg_status[1]
+    return failure_str
+
+
+@app.route('/register/group', methods=['POST'])
+def newgroup():
+    content = request.json
+    groupname = content['groupname']
+    if reg_status[0]:
+        return "Success"
     failure_str = "Register failure: " + reg_status[1]
     return failure_str
