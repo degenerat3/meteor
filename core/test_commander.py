34c6b6a1a76a0963d3c03082af57577e1c620c14
degenerat3 <james.maskelony@gmail.com>
2019-06-26 23:13:25 -0400

    fix commander

--- core/test_commander.py
+++ core/test_commander.py
@@ -6,16 +6,17 @@ server = "http://localhost:9999"
 header = {'Content-type': 'application/json'}
 data = {"groupname": "Webservers"}
 request = requests.post(server + "/register/group", headers=header, data=json.dumps(data))
-print(request.body)
+print(request.text)
 
 header = {'Content-type': 'application/json'}
 data = {"hostname": "192.69.4.20", "interface": "eth69", "groupname": "Webservers"}
 request = requests.post(server + "/register/host", headers=header, data=json.dumps(data))
-print(request.body)
+print(request.text)
 
 header = {'Content-type': 'application/json'}
 data = {"uuid": "aklsjflasdjfl", "interval": 60, "delta": 5, "hostname": "192.69.4.20"}
 request = requests.post(server + "/register/bot", headers=header, data=json.dumps(data))
-print(request.body)
+print(request.text)
 
 request = requests.get(server + "/dumpdb")
+print(request.text)
