210c4f844bec40b06bbd53d5341ed1def78093dd
degenerat3 <james.maskelony@gmail.com>
2019-07-04 15:08:57 -0400

    cleanup

--- core/dbtest_write.py
+++
@@ -1,8 +0,0 @@
-from database import *
-tgroup = Group("testgroup")
-thost = Host("test_hostname.com", "eth0", tgroup.id)
-tbot = Bot("aaaaaaaaaaaaaaaaaa", 60, 3, thost.id)
-session.add(tgroup)
-session.add(thost)
-session.add(tbot)
-session.commit()
