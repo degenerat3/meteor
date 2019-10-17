07a8454488048b6e3e67627acb5b658c9e1e360c
degenerat3 <james.maskelony@gmail.com>
2019-10-17 08:27:49 -0400

    add userdb to volume

--- modules/daddy_tops/daddy_app.py
+++ modules/daddy_tops/daddy_app.py
@@ -4,6 +4,6 @@ from daddy_app import app
 from daddy_app.views import initDB
 
 if __name__ == '__main__':
-    if not os.path.exists('db.sqlite'):
+    if not os.path.exists('/etc/dbfiles/daddytopsdb.sqlite'):
         initDB()
     app.run(debug=True, host="0.0.0.0", port=8888)
