6a78ff217539600dd6a73b04dd666d54fa0aa981
degenerat3 <james.maskelony@gmail.com>
2019-10-17 18:43:54 -0400

    fix var declarations

--- clients/build.sh
+++ clients/build.sh
@@ -11,11 +11,11 @@ read -p 'Output binary name: ' outputbin
 echo "[+] Replacing variables..."
 if [ $payloadvar == "tcp" ]
 then
-    $payloadpath="petrie/petrie.go"
+    payloadpath="petrie/petrie.go"
  
 elif [ $payloadvar == "web" ]
 then
-    $payloadpath="little_foot/little_foot.go"
+    payloadpath="little_foot/little_foot.go"
  
 else
     echo "unknown payload type"
