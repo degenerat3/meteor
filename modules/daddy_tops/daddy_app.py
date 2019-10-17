346a58e71873a6b38af551d6c19103bc92c340a6
degenerat3 <james.maskelony@gmail.com>
2019-10-16 23:20:06 -0400

    fix views import

--- modules/daddy_tops/daddy_app.py
+++ modules/daddy_tops/daddy_app.py
@@ -1,7 +1,7 @@
 import os
 from flask_sqlalchemy import SQLAlchemy
 from daddy_app import app
-from views import initDB
+from daddy_app.views import initDB
 
 if __name__ == '__main__':
     if not os.path.exists('db.sqlite'):
