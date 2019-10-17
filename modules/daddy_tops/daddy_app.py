d2aedb97e2cf5bc03ee3adfab5363913a6b3c6ad
degenerat3 <james.maskelony@gmail.com>
2019-10-16 23:03:12 -0400

    try to fix auth

--- modules/daddy_tops/daddy_app.py
+++ modules/daddy_tops/daddy_app.py
@@ -3,5 +3,5 @@ from daddy_app import app
 
 if __name__ == '__main__':
     if not os.path.exists('db.sqlite'):
-        db.create_all()
+        initDB()
     app.run(debug=True, host="0.0.0.0", port=8888)
