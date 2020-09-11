a951ac9a99ed3071ac06f1927253e67037a81353
degenerat3 <james.maskelony@gmail.com>
2020-09-11 12:48:14 -0400

    fix DT compilation

--- meteor/listeners/daddy_tops/api.go
+++ meteor/listeners/daddy_tops/api.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/degenerat3/meteor/meteor/pbuf"
+	"github.com/golang/protobuf/proto"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -9,13 +11,15 @@ import (
 
 func status(w http.ResponseWriter, r *http.Request) {
 	resp, err := http.Get("http://172.69.1.1:9999/status")
+	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	stat := &mcs.MCS{}
+	proto.Unmarshal(data, stat)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Fprintf(w, string(body))
+	fmt.Fprintf(w, string(stat.GetDesc()))
 }
